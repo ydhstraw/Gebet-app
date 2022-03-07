3a2e774d82ce0dc67c983610b2a532ddfeac9ca2
MjsTy <yudhistira.wardhana@gmail.com>
2022-03-07 20:54:37 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'YudhistiraAremaputraWardhana_00000036572_IF670_AL_UTS',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
